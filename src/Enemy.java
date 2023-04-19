397b9a1adb9f7d91cb1f10441f6313ffc29baa98
Isaac <isaac.noriega@iteso.mx>
2023-04-19 09:33:40 -0600

    JUeguito uWu

---
+++ src/Enemy.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+
+public class Enemy {
+    Game game;
+
+    //Variables de la Hitbox
+    Area head,feets,enemy;
+    //Variables para la dimension del personaje
+    int widthEnemy=38,heightEnemy=38;
+    //Coordenadas iniciales del enemigo
+    static int x_init=1300,y_init=340;
+    //Coordenadas para manipular al enemigo
+    static int x_aux=-4;
+
+    public Enemy(Game game){
+        this.game=game;
+    }
+
+    public void toMove(){
+        if(x_init<=100){
+            Game.score++;
+            x_init=Constants.WINDOW_WIDTH;
+            if(Game.score==3 |Game.score==6| Game.score==9| Game.score==12 ){
+                x_aux+=-2;
+                Game.level++;
+            }
+        }else{
+            if (colision()){
+                if(Game.lives==0){
+                    game.GameOver();
+                }else{
+                    game.LoseLives();;
+                }
+            }else{
+            x_init+=x_aux;
+            }
+        }
+    }
+
+    public void paint(Graphics2D g2){
+        ImageIcon enemy=new ImageIcon(getClass().getResource("/Multimedia/goomba.gif"));
+        g2.drawImage(enemy.getImage(),x_init,y_init,widthEnemy,heightEnemy,null);
+
+    }
+
+    public Area getBounds(){
+        Ellipse2D forma1 = new Ellipse2D.Double(x_init,y_init,widthEnemy,heightEnemy);
+        enemy=new Area(forma1);
+        return enemy;
+
+    }
+    public boolean colision(){
+        Area areaA=new Area(game.player.getBounds());
+        areaA.intersect(getBounds());
+        return !areaA.isEmpty();
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.security.Principal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class Main {
+    public static int restartGame=-1;
+    public static void main(String[] args) {
+       //JOptionPane.showMessageDialog(null, "READY?");
+
+        JFrame window = new JFrame("Juego estilo dinosaurio");
+        Game game1 = new Game();
+        //Propiedades de la ventana
+        window.add(game1);
+        window.setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        window.setLocation(Constants.WINDOW_POSITION_X, Constants.WINDOW_POSITION_Y);
+        window.setVisible(true);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        while (true) {
+            if(Game.gameOver){
+                restartGame=JOptionPane.showConfirmDialog(null,"You lose , RESTART?","You lose",JOptionPane.YES_NO_OPTION);
+                if (restartGame==0){
+                    restartValues();
+                }else if(restartGame==1){
+                    System.exit(restartGame);
+                }
+            }else {
+                game1.repaint();
+                try {
+                    Thread.sleep(12);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if (Game.loseLives==true){
+                    JOptionPane.showMessageDialog(null,"Carefull!!!");
+                    Game.loseLives=false;
+                    Game.lives--;
+                    Player.y_init=310;
+                    Player.jumping=false;
+                    Enemy.x_init=1300;
+
+                }
+            }
+        }
+    }
+
+    public static void restartValues(){
+        Game.gameOver=false;
+        Enemy.x_aux=-4;
+        Game.score=0;
+        Game.lives=3;
+        Game.level=1;
+        restartGame=-1;
+        Enemy.x_init=1300;
+
+    }
+}
